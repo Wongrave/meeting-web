8a17c3d945a515d69780164a7b7a77b46b0af20d
Lauro Tomiatti <tomiatti1@hotmail.com>
2019-05-03 17:07:21 -0300

    comecando

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: "pd-navbar",
+    templateUrl: "navbar.component.html"
+})
+
+export class NavbarComponent {
+
+    name='Meeting';
+
+
+
+
+}
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: "pd-user",
+    templateUrl: "user.component.html"
+})
+export class UserComponent {
+
+    
+
+
+}
