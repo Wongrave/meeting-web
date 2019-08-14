7b1f89000e06186210b501a7efca3555aee315d2
wongrave <tomiatti@hotmail.com>
2019-08-13 22:42:00 -0300

    something something stars

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { UserComponent } from './users/user/user.component';
 import { PropositionFormComponent } from './data-environment/propositions/proposition-form/proposition-form.component';
 import { PropositionListComponent } from './data-environment/propositions/proposition-list/proposition-list.component';
+import { PropositionComponent } from './data-environment/propositions/proposition/proposition.component';
 
 const routes: Routes = [
     { path: 'user/84', component: PropositionListComponent }, 
-    { path: 'proposition/add', component: PropositionFormComponent },
+    { path: 'proposition/add', component: PropositionComponent },
     { path: 'users/84', component: UserComponent }, 
     
 ];
