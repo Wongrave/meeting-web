c730e8edc8d673e2cc2e06de160c063cf9482d6f
Renan-NO <57843031+Renan-NO@users.noreply.github.com>
2020-01-28 17:00:50 -0300

    criacao cadastro criterio

--- src/app/data-environment/workspace/workspace.component.ts
+++ src/app/data-environment/workspace/workspace.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from 'src/app/core/auth/auth.service';
-import { formatDate } from '@angular/common';
+import { formatDate, Location } from '@angular/common';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { UserService } from 'src/app/core/user/user.service';
 import { User } from 'src/app/core/user/user';
@@ -20,6 +20,10 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 export class WorkspaceComponent implements OnInit {
 
   proposition: Proposition;
+  newFactorTag = "";
+  newFactorDescription = "";
+  newFactorSummary = "";
+  newFactorSelected = false;
   username: string;
   factors: Factor[] = []
 
@@ -28,11 +32,12 @@ export class WorkspaceComponent implements OnInit {
     private http: HttpClient,
     private auth: AuthService,
     private formBuilder: FormBuilder,
+    private location: Location,
     private userService: UserService,
     private propositionService: PropositionService,
     private workspaceService: WorkspaceService,
     private modalService: NgbModal ){ }
-    
+
     closeResult: string;
 
     open(content) {
@@ -41,7 +46,7 @@ export class WorkspaceComponent implements OnInit {
       }, (reason) => {
         this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
-    } 
+    }
 
     private getDismissReason(reason: any): string {
       if (reason === ModalDismissReasons.ESC) {
@@ -51,6 +56,20 @@ export class WorkspaceComponent implements OnInit {
       } else {
         return  `with: ${reason}`;
       }
+    }
+
+    newFactor(newFactorTag: string, newFactorDescription: string, newFactorSummary: string, newFactorSelected: boolean) {
+
+      this.workspaceService
+        .newFactor(newFactorTag, newFactorDescription, newFactorSummary, newFactorSelected, this.proposition);
+
+        this.modalService.dismissAll();
+
+        this.router.navigateByUrl('/refresh', {skipLocationChange: true}).then(() => {
+          console.log(decodeURI(this.location.path()))
+          this.router.navigate([decodeURI(this.location.path())])
+        });
+
     }
 
   ngOnInit(): void {
